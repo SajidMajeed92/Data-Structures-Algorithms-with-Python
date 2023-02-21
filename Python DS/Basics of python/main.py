fff2fc8e742464aac5640c4054126bb95dfc729a
SajidMajeed92 <sajidmajeed.ist@gmail.com>
2023-02-21 13:42:25 +0500

    Basic Print & Input statements

---
+++ Python DS/Basics of python/main.py
@@ -0,0 +1,7 @@
+print(2+202+307-200)
+print(2/202+307-200)
+print("Welcome to Data Structures & Algorithms Course(UCC-227205)")
+course_name = "DSA Python Class"
+print(course_name)
+course_name = input("Enter your course Name:")
+print(course_name)
